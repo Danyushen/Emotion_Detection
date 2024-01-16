a0c0b5120c2cfb5283735aa4f41754fc336aa631
Jan Dufek <jan.dufek2002@gmail.com>
2024-01-16 12:07:35 +0100

    Integrated timm efficientnet

--- src/models/model.py
+++ src/models/model.py
@@ -1,33 +1,41 @@
+import timm
 import torch
 import torch.nn as nn
-import torchvision.models as models
 
 
 # Define a PyTorch model using EfficientNetV2 backbone
 class EfficientNetV2Model(nn.Module):
-    def __init__(self, num_classes=6):
-        super(EfficientNetV2Model, self).__init__()
-        self.base_model = models.efficientnet_v2_m(num_classes=1000, weights='DEFAULT')
-        # Remove the classification head, leave only the backbone
-        self.base_model.classifier = nn.Identity()
-        # Freeze the weights of the backbone
-        for param in self.base_model.parameters():
-            param.requires_grad = False
 
-        # Add custom layers for classification
-        self.flatten = nn.Flatten()
-        self.dropout = nn.Dropout(0.5)
-        self.fc = nn.Linear(1280, num_classes)  # Assuming EfficientNetV2 backbone with 1280 output channels
+    def __init__(self, num_classes=6):
+        super().__init__()
+
+        # load pretrained resolution net model from timm as a backbone
+        self.base_model = timm.create_model('tf_efficientnet_b0.aa_in1k', pretrained=True)
+        num_features = self.base_model.classifier.in_features
+        
+        # modify the last layer to fit the number of classes
+        self.base_model.classifier = nn.Linear(num_features, num_features)
 
     def forward(self, x: torch.Tensor)-> torch.Tensor:
-        x = self.base_model(x)
-        x = self.flatten(x)
-        x = self.dropout(x)
-        x = self.fc(x)
-        return x
+        return self.base_model(x)
 
-# Create an instance of the PyTorch model
-model = EfficientNetV2Model()
 
-# Print the summary
-#print(model)
+if __name__ == '__main__':
+
+    criterion = nn.CrossEntropyLoss()
+
+    # test the model
+    model = EfficientNetV2Model()
+
+    # load a sample image and label batch
+    x = torch.randn(10, 3, 90, 90)
+    y = torch.randint(0, 6, (10,))
+    
+    # forward pass
+    preds = model(x)
+
+    # calculate loss
+    loss = criterion(preds, y)
+    print(loss)
+
+
